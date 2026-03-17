f6e3b3ece846402c6e930ec557f188fc30d47191
lloyd Larbi <lloyd.larbi@hva.nl>
2026-03-17 20:32:37 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// Suppress React 18.3+ defaultProps deprecation warnings from @nextui-org/react v1
+const originalError = console.error;
+console.error = (...args: Parameters<typeof console.error>) => {
+    if (typeof args[0] === "string" && args[0].includes("Support for defaultProps will be removed")) return;
+    originalError(...args);
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <NextThemesProvider attribute="class" defaultTheme="system">
