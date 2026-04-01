cbbd9eea63d0daa9a1c92dcc6b6d520459f745d7
lloyd Larbi <lloyd.larbi@hva.nl>
2026-04-01 23:03:05 +0200

    added feedback part 2 navbar fix

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -72,7 +72,7 @@ export const Navbar = () => {
                 src="/logo.png"
                 alt="EARASERS Logo"
                 width={200}    // Pas dit aan naar de gewenste breedte
-                height={150}    // Pas dit aan naar de gewenste hoogte
+                height={200}    // Pas dit aan naar de gewenste hoogte
                 priority       // Zorgt dat het logo direct geladen wordt (LCP)
                 className={styles.logoImage}
             />
